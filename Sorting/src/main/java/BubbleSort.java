4eb9b8e7330d63b5b1fd8712244dd1a45489d3bf
codingfox <carinalxx@gmail.com>
2021-05-18 14:34:16 -0700

    InputContainsMultipleElements_shouldReturnSortedList passes

--- Sorting/src/main/java/BubbleSort.java
+++ Sorting/src/main/java/BubbleSort.java
@@ -3,5 +3,22 @@ public class BubbleSort {
         if(array.length < 2) {
             return;
         }
+        boolean isSorted = false;
+        int lastUnsorted = array.length - 1;
+        while(!isSorted) {
+            isSorted = true;
+            for(int i = 0; i < lastUnsorted; i++) {
+                if(array[i]> array[i+1]) {
+                    swap(array, i, i+1);
+                    isSorted = false;
+                }
+            }
+            lastUnsorted--;
+        }
+    }
+    public void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
--- Sorting/src/test/java/BubbleSortTests.java
+++ Sorting/src/test/java/BubbleSortTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -27,5 +28,13 @@ public class BubbleSortTests {
         bubbleSort.bubblesort(array);
         assertEquals(1, array.length);
         assertEquals(4, array[0]);
+    }
+
+    @Test
+    void InputContainsMultipleElements_shouldReturnSortedList() {
+        int[] actual = {8, 0, 3, 7, 1};
+        int[] expected = {0, 1, 3, 7, 8};
+        bubbleSort.bubblesort(actual);
+        assertArrayEquals(expected, actual);
     }
 }
